67c11713b0841df46ed27bfa34cd9f08ed19cf80
ThunderSmotch <2165764+ThunderSmotch@users.noreply.github.com>
2019-02-12 01:00:24 +0000

    Second Commit - Project imported to IDEA

---
+++ src/main/java/thundersmotch/sophia/ExampleMod.java
@@ -0,0 +1,31 @@
+package thundersmotch.sophia;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "sophia";
+    public static final String NAME = "Sophia";
+    public static final String VERSION = "0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
